59e09864510b34cda42f195bced1bb289a08f153
hks <sutt2000@naver.com>
2021-01-01 02:50:06 +0900

    몽고db설치

--- server/app.js
+++ server/app.js
@@ -1,5 +1,16 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config'
+
+
+const app = express();
+const { MONGO_URI } = config;
+
+mongoose.connect(MONGO_URI ,{    
+      useNewUrlParser: true,
+      useUnifiedTopology: true,    
+}).then(()=> console.log("MongoDB connection Success!!"))
+.catch((e) => console.log(e)); 
 
 app.get('/')
 
