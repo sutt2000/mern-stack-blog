23a4b2f210426ec4791920047bfe73c17f439888
hks <sutt2000@naver.com>
2021-01-01 17:46:01 +0900

    postman 활용

--- server/app.js
+++ server/app.js
@@ -1,38 +1,38 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import config from './config'
-
+import express from "express";
+import hpp from "hpp";
+import cors from "cors";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import config from "./config";
+//Routes
+import postsRoutes from "./routes/api/post";
 
 const app = express();
 const { MONGO_URI } = config;
 
-// 
-app.use(hpp())
+app.use(hpp());
 // 서버보안 설정
-app.use(helmet())
-
+app.use(helmet());
 // 브라우져가 다른 도메인이나 서비스가 요청하는 것으로
 //                   모두 허락 한다
-app.use(cors({origin: true, Credential:true }))
+app.use(cors({ origin: true, Credential: true }));
 // 로그확인
-app.use(morgan("dev"))
-
+app.use(morgan("dev"));
 //json 형태로 해석해 주세용~
-app.use(express.json())
+app.use(express.json());
 
-
-
-
-
-mongoose.connect(MONGO_URI ,{    
-      useNewUrlParser: true,
-      useUnifiedTopology: true,    
-}).then(()=> console.log("MongoDB connection Success!!"))
-.catch((e) => console.log(e)); 
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connection Success!!"))
+  .catch((e) => console.log(e));
 
 //라우터란 분기점으로 책깔피같은것이다!!
-app.get('/')
+app.get("/");
+app.use("/api/post", postsRoutes);
 
 export default app;
-
